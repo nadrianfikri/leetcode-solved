ac745914a33c80f502eabe7a010af03824f5cbbf
nadrianfikri <82949202+nadrianfikri@users.noreply.github.com>
2023-07-30 22:23:51 +0700

    Time: 133 ms (99.51%), Space: 50.5 MB (88.17%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,9 +3,18 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    const convertedNumber = x.toString()
-    const reversed = convertedNumber.split('').reverse().join('')
-    return convertedNumber === reversed
+    let final = 0;
+    let num = x;
+    while (num > 0) {
+		// Moves last digit from the back
+        const lastDigit = num % 10;
+		// We start adding it from the front
+        final = final * 10 + lastDigit;
+		// If don't use Math.floor, we would get an answer of Infinity since it will get smaller but never hit 0.
+        num = Math.floor(num / 10);
+    }
+   
+    return x === final;
     
     
 };
