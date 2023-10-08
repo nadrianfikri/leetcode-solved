943f1c17606c41d81b595230dc9d28234983892e
nadrianfikri <82949202+nadrianfikri@users.noreply.github.com>
2023-10-08 19:51:06 +0700

    Time: 50 ms (64.30%), Space: 41.8 MB (86.09%) - LeetHub

--- 1185-day-of-the-week/1185-day-of-the-week.js
+++ 1185-day-of-the-week/1185-day-of-the-week.js
@@ -4,10 +4,32 @@
  * @param {number} year
  * @return {string}
  */
-var dayOfTheWeek = function(day, month, year) {
-    const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    const date = new Date(`${year}-${month}-${day}`)
-    const d = date.getDay()
+// var dayOfTheWeek = function(day, month, year) {
+//     const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+//     const date = new Date(`${year}-${month}-${day}`)
+//     const d = date.getDay()
     
-    return dayNames[d]
+//     return dayNames[d]
+// };
+
+var dayOfTheWeek = function(day, month, year) {
+    let weekDay = ["Saturday","Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+
+    if (month < 3) {
+        month += 12;
+        year -= 1;
+    }
+    
+    let k = year%100
+    let j = Math.floor(year/100)
+    let q =day
+    let m= month;
+    
+    return weekDay[calculate(q,k,j,m)]
+
+function calculate(q,k,j,m){
+    let h = q+Math.floor(13*(m+1)/5)+k+Math.floor(k/4)+Math.floor(j/4)+5*j
+    return h%7
+}
 };
+
